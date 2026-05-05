01b757733647fb2a7ff8b01500f692ac0b50fc80
Dionysis <dionysiskatsetis@gmail.com>
2026-05-05 21:23:34 +0300

    working but ugly

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from utils.data_loader import initialize_session_state
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .engine import run_market_clearing_ptdf_elastic
+from .engine import run_market_clearing
 from .visualization import draw_network_graph
 from .data_loader import initialize_session_state, validate_network_data
 from .network_tools import calculate_ptdf
